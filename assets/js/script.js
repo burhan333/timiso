4a9b9f8c649019b19b207ee63445623335a9cc51
burhan <em.burhan@yahoo.com>
2020-09-13 16:00:40 +0500

    Commiting complete web

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const popup = document.getElementById('popup');
+
+function Open ()
+{
+    popup.style.display = 'flex';
+}
+
+function Close ()
+{
+    popup.style.display = 'none';
+}
